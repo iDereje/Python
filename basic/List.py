01a1de3e63c0a902b5a02f8cefa5c2897aec4dcb
David Dereje Mesganaw <derejeisack@gmail.com>
2023-04-02 03:00:11 +0300

    "Day 2 Python"

---
+++ basic/List.py
@@ -0,0 +1,24 @@
+favorit_colors = ["red", "blue", "green"]
+print(favorit_colors)
+
+# #append() method in python is a method that add an element to the end of the list
+# #syntax
+# # list.append(element)
+
+# #example
+favorit_colors.append("yellow")
+print(favorit_colors)
+
+
+favorit_colors [1] = "yellow"
+print(favorit_colors)
+if favorit_colors[1] != "red":
+    def favorite_colors():
+        favorit_colors[1] = "red"
+        print(favorit_colors)
+
+
+    favorite_colors()
+    print(favorit_colors)
+else:
+  print(favorit_colors)
